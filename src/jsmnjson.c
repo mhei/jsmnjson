67d119ae62dfab59e44d62f89d1efa3ed92bb421
Michael Heimpold <mhei@heimpold.de>
2015-06-14 01:45:35 +0200

    Add jsmnjson_get_size and jsmnjson_get_by_key
    Signed-off-by: Michael Heimpold <mhei@heimpold.de>

--- src/jsmnjson.c
+++ src/jsmnjson.c
@@ -1,8 +1,98 @@
 #include <string.h>
 #include <errno.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdarg.h>
 #include <jsmn.h>
 #include "jsmnjson.h"
+
+#define MAX_DEPTH 8
+
+static int __dbg(int level, const char *format, ...)
+{
+	va_list ap;
+	int rv1, rv2;
+
+	rv1 = printf("%*s[%d] ", 4 * (MAX_DEPTH - level), " ", level);
+	if (rv1 < 0)
+		return rv1;
+
+	va_start(ap, format);
+	rv2 = vprintf(format, ap);
+	va_end(ap);
+
+	if (rv2 < 0)
+		return rv2;
+
+	return rv1 + rv2;
+}
+
+#if 0
+static int __jsmnjson_get_size(jsmntok_t *t, int level)
+{
+	int i, size = 1;
+	jsmntok_t *p = t + 1;
+
+	__dbg(level, "entering\n");
+
+	if (level == 0) {
+		__dbg(level, "returning -1 (max level)1\n");
+		return -1;
+	}
+
+	if (t->size == 0) {
+		__dbg(level, "returning 1\n");
+		return 1;
+	}
+
+	for (i = 0; i < t->size; i++) {
+		int subsize;
+
+		__dbg(level, "Going sub (%d)\n", ((long int)p - (long int)t)/sizeof(jsmntok_t) );
+
+		subsize = __jsmnjson_get_size(p, level - 1);
+		if (subsize == -1) {
+			__dbg(level, "got max level, propagating\n");
+			return -1;
+		}
+
+		__dbg(level, "setting pointer + %d\n", subsize);
+		p += subsize;
+		size += subsize;
+	}
+
+	__dbg(level, "returning %d\n", size);
+	return size;
+}
+#endif
+
+static int __jsmnjson_get_size(jsmntok_t *t, int level)
+{
+	int i, size = 1;
+	jsmntok_t *p = t + 1;
+
+	if (level == 0)
+		return -1;
+
+	if (t->size == 0)
+		return 1;
+
+	for (i = 0; i < t->size; i++) {
+		int subsize = __jsmnjson_get_size(p, level - 1);
+		if (subsize == -1)
+			return -1;
+
+		p += subsize;
+		size += subsize;
+	}
+
+	return size;
+}
+
+int jsmnjson_get_size(jsmntok_t *t)
+{
+	return __jsmnjson_get_size(t, MAX_DEPTH);
+}
 
 long int jsmnjson_get_int(jsmntok_t *t, const char *json)
 {
@@ -41,4 +131,53 @@ int jsmnjson_strcmp(jsmntok_t *t, const char *json, const char *s2)
 		return strncmp(&json[t->start], s2, t->end - t->start);
 
 	return -1;
+}
+
+jsmntok_t *__jsmnjson_get_by_key(jsmntok_t *t, const char *json, const char *key)
+{
+	int i, size;
+	jsmntok_t *p = t + 1;
+
+
+	if ((t->type != JSMN_OBJECT) || (t->size == 0)) {
+		errno = EINVAL;
+		return NULL;
+	}
+
+	for (i = 0; i < t->size; i++) {
+		if (jsmnjson_strcmp(p, json, key) == 0)
+			return p + 1;
+
+		size = __jsmnjson_get_size(p, MAX_DEPTH);
+		if (size == -1)
+			return NULL;
+
+		p += size;
+	}
+
+	return NULL;
+}
+
+jsmntok_t *jsmnjson_get_by_key(jsmntok_t *t, const char *json, const char *key)
+{
+	char buf[128];
+	int level = MAX_DEPTH;
+	jsmntok_t *p = t;
+	char *s = buf;
+	char *d;
+
+	if (strlen(key) >= ARRAY_SIZE(buf))
+		return NULL;
+
+	strcpy(buf, key);
+
+	while (level-- && (d = strchr(s, '/'))) {
+		*d = '\0';
+		p = __jsmnjson_get_by_key(p, json, s);
+		if (p == NULL)
+			return NULL;
+		s = d + 1;
+	}
+
+	return __jsmnjson_get_by_key(p, json, s);
 }
--- src/jsmnjson.h
+++ src/jsmnjson.h
@@ -24,6 +24,8 @@ extern "C" {
 long int jsmnjson_get_int(jsmntok_t *t, const char *json);
 int jsmnjson_strncopy(char *dest, size_t n, jsmntok_t *t, const char *json);
 int jsmnjson_strcmp(jsmntok_t *t, const char *json, const char *s2);
+int jsmnjson_get_size(jsmntok_t *t);
+jsmntok_t *jsmnjson_get_by_key(jsmntok_t *t, const char *json, const char *key);
 
 #ifdef __cplusplus
 }
